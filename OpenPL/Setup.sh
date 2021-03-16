c1c1e4d3632017adde5348bedbc8e685707ee582
James Kelly <jameskelly21699@gmail.com>
2021-03-16 14:30:47 +0000

    Fix and Tidy Setup.sh

--- OpenPL/Setup.sh
+++ OpenPL/Setup.sh
@@ -6,11 +6,16 @@
 # Colours
 NOCOLOR='\033[0m'
 RED='\033[0;31m'
+BLUE='\033[0;34m'
 
 # Move into directory we were invoked in
 cd "`dirname "$0"`"
 
 echo "${RED}Generating OpenPL${NOCOLOR}"
+
+# Stop advice for detatched heads
+# We're not cloning the projects to make commits so this is fine
+git config --global advice.detachedHead false
 
 # If libigl isn't downloaded, clone
 if [ ! -d "External/libigl/" ]
@@ -31,12 +36,17 @@ then
     mkdir glad
     mkdir glfw
 
+    echo "${BLUE}Downloading eigen${NOCOLOR}"
     git clone https://github.com/libigl/eigen.git --depth 1 eigen
+    
+    echo "${BLUE}Downloading glad${NOCOLOR}"
     git clone https://github.com/libigl/libigl-glad.git --depth 1 glad
+    
+    echo "${BLUE}Downloading glfw${NOCOLOR}"
     git clone https://github.com/glfw/glfw.git --depth 1 glfw
 
     # Go back up a folder for the following commands to work
-    cd ../../
+    cd ../../../
 fi
 
 # If matplot isn't downloaded, clone
@@ -45,7 +55,7 @@ then
     echo "${RED}Downloading matplotplusplus${NOCOLOR}"
 
     cd External/
-    mdir matplotplusplus
+    mkdir matplotplusplus
     git clone https://github.com/alandefreitas/matplotplusplus.git --branch v1.0.1 --depth 1 matplotplusplus
     
     cd ../
